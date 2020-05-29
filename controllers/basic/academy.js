e137c257ea48a22b3ed1db37791e49082eea563a
张云 <zhy521up@163.com>
2020-05-29 17:59:56 +0800

    add

--- controllers/basic/academy.js
+++ controllers/basic/academy.js
@@ -1,13 +1,12 @@
 const db = require('../../config/db')
 
 const sql = {
-    insert: `INSERT INTO cam_academy(id, name, age) VALUES(?,?,?)`,
+    insert: `INSERT INTO cam_academy(name, code, academy_type, intro) VALUES(?,?,?,?)`,
     update: `UPDATE cam_academy SET name=?, age=? WHERE id=?`,
     delete: `DELETE FROM cam_academy WHERE id=?`,
     queryById: `SELECT * FROM cam_academy WHERE id=?`,
     count:`select count(*) as count from cam_academy`,
     queryAll: `SELECT * FROM cam_academy`
-    //queryAll: (page, rows) => `SELECT * FROM cam_academy limit ${rows * (page - 1)},${rows}`
 }
 
 class Academy {
@@ -15,20 +14,25 @@ class Academy {
      * 列表带分页和模糊查询
      * */
     list(req, res, next) {
-        console.log(req.body)
         const { keywords, rows, page } = req.body
-        let content = []
+        const params = []
         if (keywords) {
             sql.queryAll += ` WHERE code LIKE ? or name like ?`
             content.push(`%${keywords}%`, `%${keywords}%`)
         }
         sql.queryAll += `  limit ?,?`
-        content.push(rows * (page - 1) , rows)
+        params.push(rows * (page - 1) , rows)
         db.query(sql.count, null, function (err, result) {
             if (err) throw err;
             const count = result[0].count
-            db.query(sql.queryAll, content, function (err, result) {
+            db.query(sql.queryAll, params, function (err, result) {
                 if (err) throw err;
+                result.forEach(item =>{
+                    item.id = item.id + ''
+                    item.createDate = item.create_date 
+                    item.modifyDate = item.modify_date
+                    item.createDate = item.create_date
+                })
                 res.send({
                     status: 1,
                     state: 'success',
@@ -36,12 +40,23 @@ class Academy {
                     total: count,
                     data: result
                 })
-            });
+            })
 
-        });
-    };
+        })
+    }
     add(req, res, next){
-
+        console.log(req.body)
+        const { name, code, academyType, intro } = req.body
+        const params = [name, code, academyType, intro]
+        db.query(sql.insert, params, function (err, result){
+            //if (err) throw err;
+            res.send({
+                status: 1,
+                state: 'success',
+                message: '操作成功',
+                data: []
+            })
+        })
     }
 
 }
